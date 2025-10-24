3f278def79aa60464b489df39215564e51258ab6
godccino <supitchaklanpradit@outlook.com>
2025-10-24 07:42:21 +0700

    completed database setup

--- README.md
+++ README.md
@@ -4,3 +4,29 @@
 -   Create custom Button component with variants (Mimic from shadcn)
 -   Setup proxy in vite
 -   Implement Hono RPC
+-   Setup and run Docker container locally
+-   Database setup using Drizzle ORM
+
+### Local Dev
+
+1. Install dependencies
+
+```sh
+bun install
+```
+
+2. Copy `.env.example` to your own `.env` file
+3. Setup database
+
+```sh
+bun run db:up
+bun run db:migrate
+```
+
+4. Running
+
+```sh
+bun run dev
+```
+
+open http://localhost:3000
---
+++ scripts/seed-local-db.script.ts
@@ -0,0 +1,34 @@
+import { db, pool } from "../server/db/db";
+import * as schema from "../server/db/schema";
+import { seed } from "drizzle-seed";
+
+const seedDb = async () => {
+    await seed(db, schema).refine((func) => ({
+        todos: {
+            columns: {
+                title: func.valuesFromArray({ values: ["Buy groceries", "Walk the dog", "Read a book", "Write code", "Exercise"] }),
+                description: func.valuesFromArray({
+                    values: [
+                        undefined,
+                        "Milk, Bread, Eggs",
+                        "30-minute walk in the park",
+                        "Finish reading 'Clean Code'",
+                        "Work on Hono project",
+                        "30 minutes of cardio",
+                    ],
+                }),
+                // completed: func.valuesFromArray({ values: [false, true, false, false, true] }),
+            },
+        },
+    }));
+};
+
+seedDb()
+    .then(() => {
+        console.log("Seeded local database successfully.");
+        return pool.end();
+    })
+    .catch((error) => {
+        console.error("Error seeding local database:", error);
+        return pool.end();
+    });
