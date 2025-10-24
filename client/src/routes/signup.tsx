9e1c8310e997ed902f6f5e5d593df05773e91213
godccino <supitchaklanpradit@outlook.com>
2025-10-24 11:51:54 +0700

    added signup route

--- client/src/routes/signup.tsx
+++ client/src/routes/signup.tsx
@@ -5,5 +5,5 @@ export const Route = createFileRoute('/signup')({
 })
 
 function RouteComponent() {
-  return <div>Hello "/signup"!</div>
+  return <div className="route-starter">Hello signup</div>
 }
